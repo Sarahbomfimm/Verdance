b664a4742003b134d7dcbacb919a8165e0411a5e
Sarah Bomfimm <sarahbomfimm24@gmail.com>
2026-06-24 08:39:07 -0300

    icone smartphone

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -76,6 +76,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "icon", type: "image/png", href: "/favicon.png" },
+      { rel: "apple-touch-icon", href: "/favicon.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
